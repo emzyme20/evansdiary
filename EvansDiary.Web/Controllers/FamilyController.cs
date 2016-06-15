744739c7f9ad2d70c1bf1ba49e1c27ea829b9d41
Emma Middlebrook <emma@vouchercloud.com>
2016-06-15 15:46:49 +0100

    can scatter polaroid family pics.

--- EvansDiary.Web/Controllers/FamilyController.cs
+++ EvansDiary.Web/Controllers/FamilyController.cs
@@ -15,8 +15,8 @@ namespace EvansDiary.Web.Controllers
                     new AssociatedImage("sand_dune_jumping_jk1all", "Leap of Faith", "175"),
                     new AssociatedImage("beach_r7zxeb", "Exmouth 2011", "250"),
                     new AssociatedImage("camping_bokp4f", "Watching the Red Arrows", "250"),
-                    new AssociatedImage("torch_hxqf13", "2012 London Olympics", "400"),
-                    new AssociatedImage("prior-park-lake_lvnkwn", "Prior Park Lake", "400"),
+                    new AssociatedImage("torch_hxqf13", "2012 London Olympics", "300"),
+                    new AssociatedImage("prior-park-lake_lvnkwn", "Prior Park Lake", "300"),
                     new AssociatedImage("prior-park-selfie_pyrxw7", "Prior Park Selfie", "225"),
                     new AssociatedImage("prior-park-swings_w8bkxd", "Double Swing", "275"),
                     new AssociatedImage("sand_drawing_jldnzm", "New Quay 2016", "175")
