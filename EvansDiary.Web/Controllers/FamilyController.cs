a6873a66634a613f5fdc902e9ea97760e0a5f817
Emma Middlebrook <emma@vouchercloud.com>
2016-06-17 13:04:14 +0100

    updated some widths

--- EvansDiary.Web/Controllers/FamilyController.cs
+++ EvansDiary.Web/Controllers/FamilyController.cs
@@ -12,14 +12,14 @@ namespace EvansDiary.Web.Controllers
             {
                 Images = new[]
                 {
-                    new AssociatedImage("sand_dune_jumping_jk1all", "Leap of Faith", "175"),
+                    new AssociatedImage("sand_dune_jumping_jk1all", "Leap of Faith", "250"),
                     new AssociatedImage("beach_r7zxeb", "Exmouth 2011", "250"),
                     new AssociatedImage("camping_bokp4f", "Watching the Red Arrows", "250"),
                     new AssociatedImage("torch_hxqf13", "2012 London Olympics", "300"),
                     new AssociatedImage("prior-park-lake_lvnkwn", "Prior Park Lake", "300"),
                     new AssociatedImage("prior-park-selfie_pyrxw7", "Prior Park Selfie", "225"),
                     new AssociatedImage("prior-park-swings_w8bkxd", "Double Swing", "275"),
-                    new AssociatedImage("sand_drawing_jldnzm", "New Quay 2016", "175")
+                    new AssociatedImage("sand_drawing_jldnzm", "New Quay 2016", "225")
                 }
             };
             return View(viewModel);
